620de5b97024591e46a66938ffb51bd7f33398b1
Naveed Danish <ndanish1@my.centennialcollege.ca>
2024-03-26 18:20:01 -0400

    Add project files.

---
+++ quiz4-demo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using quiz4_demo.Models;
+using System.Diagnostics;
+
+namespace quiz4_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // Add the database access statement here
+            ViewData["DatabaseMessage"] = "Database access has already been done on April 14, 2023 by [Your Name]";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
